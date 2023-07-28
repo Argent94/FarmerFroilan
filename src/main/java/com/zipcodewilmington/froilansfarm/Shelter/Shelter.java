6c0753ef5eac94d58d2c0d4a82a3ca0f6e0d66d6
Dmytro_Romaniv <128440171+dimaromaniv@users.noreply.github.com>
2023-07-28 16:00:16 -0400

    test in shelter

--- src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Shelter.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Shelter.java
@@ -2,8 +2,5 @@ package com.zipcodewilmington.froilansfarm.Shelter;
 
 import com.zipcodewilmington.froilansfarm.Animals.Animal;
 
-public interface Shelter extends Animal<AnimalType extends Animal> {
-    public  addAnimal () {
-
-    }
+public interface Shelter  {
 }
--- src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Stable.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Stable.java
@@ -2,17 +2,37 @@ package com.zipcodewilmington.froilansfarm.Shelter;
 
 import com.zipcodewilmington.froilansfarm.Animals.Horse;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Stable {
-    private List<Horse> horses ;
+    private List<Horse> horses;
 
-    public Stable(List horses){
-        this.horses = horses;
+    public Stable(){
+        this.horses = new ArrayList<>();
     }
 
     public void addAnimal (Horse horse) {
         horses.add(horse);
     }
+    public Integer getSize () {
+        return this.horses.size();
+    }
 
-}
+    public void feeding () {
+
+    }
+    public Integer checkOutHorse() {
+
+        return null;
+    }
+    public Integer checkInHorse () {
+
+        return null;
+    }
+
+    public void clear () {
+        horses.clear();
+    }
+
+ }
