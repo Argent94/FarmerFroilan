7732e58b94d9fcfd935683663ebfccf79899bc49
maggie <snydermag@gmail.com>
2023-07-28 19:23:54 -0400

    Updated class types

--- src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Shelter.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Shelter/Shelter.java
@@ -2,7 +2,7 @@ package com.zipcodewilmington.froilansfarm.Shelter;
 
 import com.zipcodewilmington.froilansfarm.Animals.Animal;
 
-public interface Shelter extends Animal<AnimalType extends Animal> {
+public interface Shelter <AnimalType extends Animal> {
     public  addAnimal () {
 
     }
