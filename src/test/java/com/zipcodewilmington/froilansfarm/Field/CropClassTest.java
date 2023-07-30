d1b14f8e5657cbe05c7b0ec1500c4d4477029824
santos <santoslopez890@gmail.com>
2023-07-30 11:09:06 -0400

    Crop Class test skeleton added

--- src/test/java/com/zipcodewilmington/froilansfarm/Field/CropClassTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Field/CropClassTest.java
@@ -10,7 +10,34 @@ public class CropClassTest {
     }
 
     @Test
-    public void testCropClass2() {
+    public void yieldEdibleTest () {
+        Crop cropClass = new Crop();
+
+    }
+
+    @Test
+    public void getYieldTest() {
+
+        Crop cropClass = new Crop();
+
+    }
+    @Test
+    public void setYieldTest() {
+
+        Crop cropClass = new Crop();
+
+    }
+
+    @Test
+    public void fertilizeCropTest() {
+
+        Crop cropClass = new Crop();
+
+    }
+
+    @Test
+    public void beenFertilizeCropTest() {
+
         Crop cropClass = new Crop();
 
     }
