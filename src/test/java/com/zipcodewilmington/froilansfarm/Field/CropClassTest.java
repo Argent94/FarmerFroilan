29ee696d9f6a9143750c4ed5eba732fa0d9e7557
santos <santoslopez890@gmail.com>
2023-07-30 10:51:37 -0400

    TomatoPlant Tests added

--- src/test/java/com/zipcodewilmington/froilansfarm/Field/CropClassTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/Field/CropClassTest.java
@@ -1,4 +1,19 @@
 package com.zipcodewilmington.froilansfarm.Field;
+import org.junit.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class CropClassTest {
+    @Test
+    public void testCropClass() {
+        Crop cropClass = new Crop();
+
+    }
+
+    @Test
+    public void testCropClass2() {
+        Crop cropClass = new Crop();
+
+    }
+
+
 }
