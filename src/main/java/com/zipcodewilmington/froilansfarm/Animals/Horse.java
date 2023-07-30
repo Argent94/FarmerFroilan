faae14986d1debbd8d9cad7fdaa24a3aa04e816e
maggie <snydermag@gmail.com>
2023-07-30 16:12:20 -0400

    Fixed minor bugs

--- src/main/java/com/zipcodewilmington/froilansfarm/Animals/Horse.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Animals/Horse.java
@@ -2,8 +2,8 @@ package com.zipcodewilmington.froilansfarm.Animals;
 
 import com.zipcodewilmington.froilansfarm.Edible.Egg;
 import com.zipcodewilmington.froilansfarm.Field.Produce;
-import com.zipcodewilmington.froilansfarm.Ridable;
+import com.zipcodewilmington.froilansfarm.Rideable;
 
-public class Horse <Corn> extends Animal implements Ridable {
+public class Horse <Corn> extends Animal implements Rideable {
 
 }
