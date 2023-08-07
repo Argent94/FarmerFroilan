81b5f185593ea0b3d1d2605783e52d895a85c2c2
maggie <snydermag@gmail.com>
2023-08-07 15:42:55 -0400

    updated aircraft and tests

--- src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/Vehicle.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/Vehicles/Vehicle.java
@@ -3,9 +3,13 @@ package com.zipcodewilmington.froilansfarm.Vehicles;
 import com.zipcodewilmington.froilansfarm.Persons.Pilot;
 import com.zipcodewilmington.froilansfarm.Rider;
 
-public class Vehicle<RiderType extends Rider> {
+public abstract class Vehicle<RiderType extends Rider> {
 
+    RiderType rider;
 
     public void addRider(RiderType rider) {
     }
+
+
+
 }
